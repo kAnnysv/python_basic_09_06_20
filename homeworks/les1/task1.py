87f4d052de1fe7ffd88631e904954caf0fb57c6c
kAnny <valssss@yandex.ru>
2020-06-12 11:02:06 +0300

    update variables

--- homeworks/les1/task1.py
+++ homeworks/les1/task1.py
@@ -2,3 +2,6 @@ var_int = 5
 var_fl = 3.56
 var_str = 'kany'
 print(var_int, var_fl, var_str)
+age = input('Ведите свой возраст: ')
+name = input('Введите своё имя: ')
+print('Ваш возраст', age,'Ваше имя', name)
