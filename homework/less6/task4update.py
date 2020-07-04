27ce6438854ef5b219bed295632563210a8fc115
kAnny <valssss@yandex.ru>
2020-07-04 10:19:27 +0300

    update Car

---
+++ homework/less6/task4update.py
@@ -0,0 +1,58 @@
+class Car:
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        return f'{self.name} начал движение'
+
+    def stop(self):
+        return f'{self.name} остановился'
+
+    def turn(self, direction):
+        return f'{self.name} повернул на {direction}'
+
+    def show_speed(self):
+        return f'{self.name} движется со скоростью {self.speed}'
+
+
+class TownCar(Car):
+
+    def show_speed(self):
+        print(f'{self.name} движется со скоростью {self.speed}')
+        if self.speed > 60:
+            return f'У {self.name} Привышение скорости'
+
+
+class Sport_car(Car):
+    pass
+
+
+class Work_car(Car):
+
+
+    def show_speed(self):
+        print(f'{self.name} движется со скоростью {self.speed}')
+        if self.speed > 40:
+            return f'У {self.name} Привышение скорости'
+
+
+class Police_car(Car):
+    pass
+
+
+
+Ford = TownCar(70, 'Синий', 'Ford', False)
+Ferrari = Sport_car(200, 'Красный', 'Ferrari', True)
+Hino = Work_car(50, 'Зеленый', 'Hino', False)
+UAZ = Police_car(80, 'Белый', 'UAZ', True)
+
+print(Ford.go(), Ford.turn('Лево'))
+print(Ford.show_speed())
+print(Ferrari.go(), Ferrari.show_speed(), Ferrari.turn('право'))
+print(Ferrari.stop())
+print(Hino.go())
+print(Hino.show_speed())
+print(UAZ.is_police)
